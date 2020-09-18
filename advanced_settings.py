1882503c7ba6f037faab7d1996283c4ca032d30b
KeeeeeK <nugmanov.bn@phystech.edu>
2020-09-19 02:28:57 +0300

    adding advanced functions

---
+++ advanced_settings.py
@@ -0,0 +1,17 @@
+from numpy import linspace as _linspace
+
+
+def parabola_coefficients(x1, x2, x3, y1, y2, y3):
+    # ax^2+bx+c
+    a = (y3 - (x3 * (y2 - y1) + x2 * y1 - x1 * y2) / (x2 - x1)) / (x3 * (x3 - x1 - x2) + x1 * x2)
+    b = (y2 - y1) / (x2 - x1) - a * (x1 + x2)
+    c = (x2 * y1 - x1 * y2) / (x2 - x1) + a * x1 * x2
+    return a, b, c
+
+
+def draw_parabola_func(x_min, x_max, N, a, b, c):
+    def draw_parabola(ax):
+        x = _linspace(x_min, x_max, N)
+        ax.scatter(x=x, y=a * x ** 2 + b * x + c, s=1)
+
+    return draw_parabola
