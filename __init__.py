718529a452db6e7261688d082c633770e2c31a38
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2020-10-26 16:13:53 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from math import pi
-from .var import Var, GroupVar, group, ungroup
+from .var import Var, GroupVar
 from .table import TexTable, to_table
 from .plot import Figure, mnk, mnk_through0
 from .calculus_functions import sqrt, sin, cos, tg, ctg, arctg, arcctg, arcsin, arccos, \
                                 sh, ch, th, cth, arcth, arcsh, exp, ln, mean, step
+
+# Look in the advanced_functions file for additional functionality
