8a3713de8bfad342651dbf8de520e952f6112abd
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2021-03-23 02:26:24 +0300

    big update

--- plot.py
+++ plot.py
@@ -1,48 +1,117 @@
+from __future__ import annotations
+
 from functools import reduce
-from typing import Optional, Tuple, Union, Callable
+from typing import Optional, Tuple, Union, Callable, SupportsFloat, Sequence
 
 import matplotlib.patches as _mp
 import matplotlib.pyplot as plt
+from numpy import array, linspace, sqrt
 
 from .var import Var, GroupVar
 
 
 class Figure:
+    """
+    This class is made to show graphs
+    """
+
     def __init__(self, x_label: str = '', y_label: str = '', bold_axes: bool = True, zero_in_corner: bool = True,
-                 label_near_arrow: bool = True, my_func: Optional[Callable] = None):
+                 label_near_arrow: bool = True, my_func: Optional[Callable] = None,
+                 x_label_coords: Sequence[SupportsFloat] = array([1.03, -0.03]),
+                 y_label_coords: Sequence[SupportsFloat] = array([-0.06, 1]),
+                 legend_props: Optional[dict] = None):
         """
-        :param x_label: label near x axis
-        :param y_label: label near y axis
-        :param bold_axes: need of y=0 and x=0 lines
+        :param x_label: label near xt axis
+        :param y_label: label near y_exp axis
+        :param bold_axes: need of y_exp=0 and xt=0 lines
         :param zero_in_corner: need of showing (0, 0) wherever the other dots
         :param label_near_arrow: if True: labels will be shown in the corners near arrows of axis.
                                  if False: labels will be near centers of appropriate axis.
         :param my_func: function that takes an object of the matplotlib.axes._subplots.AxesSubplot class as an argument.
-        It will be called before fixing axes and adding lines. You may use it to add anything you want without using Var
-        and GroupVar.
+        It will be called before fixing axes and adding lines. You may use it to add anything you want.
+        :param x_label_coords: coordinates of placing x_label.
+        It's easy to shift x_label in this way: figure.x_label_coords+=array([0.03, -0.04])
+        :param y_label_coords: similar to x_label_coords
+        :param legend_props: dictionary for
         """
         self.x_label, self.y_label = x_label, y_label
+        self.x_label_coords, self.y_label_coords = x_label_coords, y_label_coords
         self.bold_axes = bold_axes
         self.zero_in_corner = zero_in_corner
         self.label_near_arrow = label_near_arrow
         self.my_func = my_func
+        self.legend_props: dict = legend_props if legend_props is not None else {}
+        self.alpha = 0.25  # the transparency of v_lines and h_lines
         # params for 'plot' method
         self._scatters_kwargs = []
         self._errorbars_kwargs = []
         # params for 'line' method
         self._lines_params = []
+        self._v_lines_params = []
+        self._h_lines_params = []
+        self._func_graphs_before_fixing_axes = []
+        self._func_graphs_after_fixing_axes = []
 
-    def line(self, k: Union[float, int], b: Union[float, int], colour: Optional[str] = None,
-             line_style: Optional[str] = None, label: Optional[str] = None):
+    def line(self, k: Union[float, int, Var], b: Union[float, int, Var], colour: Optional[str] = None,
+             line_style: Optional[str] = None, label: Optional[str] = None) -> Figure:
+        if isinstance(k, Var):
+            k = k.val()
+        if isinstance(b, Var):
+            b = b.val()
         self._lines_params.append((k, b, colour, line_style, label))
+        return self
 
-    def plot(self, x: GroupVar, y: GroupVar, capsize=3, s=1, c=None, marker=None, label=None) -> None:
-        x_val, x_err = x.val_err()
-        y_val, y_err = y.val_err()
+    def v_line(self, x: Union[float, int, Var], colour: Optional[str] = None, line_style: Optional[str] = None,
+               label: Optional[str] = None) -> Figure:
+        self._v_lines_params.append((x, colour, line_style, label))
+        return self
+
+    def h_line(self, y: Union[float, int, Var], colour: Optional[str] = None, line_style: Optional[str] = None,
+               label: Optional[str] = None) -> Figure:
+        self._h_lines_params.append((y, colour, line_style, label))
+        return self
+
+    def func_graph(self, func: Callable[[array], array], x_min: Union[int, float, Var], x_max: Union[int, float, Var],
+                   N: int = 1000, line_style: Optional[str] = None, colour: Optional[str] = None,
+                   label: Optional[str] = None, add_before_fixing_axes: bool = True) -> Figure:
+        """
+        todo: doc and ;сделать возможность проводить линию до края графика по оси xt влево или вправо, если указано None;
+        :param func:
+        :param x_min:
+        :param x_max:
+        :param N:
+        :param line_style:
+        :param colour:
+        :param label:
+        :param add_before_fixing_axes:
+        :return:
+        """
+        if isinstance(x_min, Var):
+            x_min = x_min.val()
+        if isinstance(x_max, Var):
+            x_max = x_max.val()
+        x = linspace(x_min, x_max, N)
+        y = func(x)
+        ({False: self._func_graphs_before_fixing_axes, True: self._func_graphs_after_fixing_axes}
+        )[add_before_fixing_axes].append((x, y, line_style, colour, label))
+        return self
+
+    def plot(self, x: Union[GroupVar, Sequence], y: Union[GroupVar, Sequence],
+             capsize=3, s=1, colour=None, marker=None, label=None) -> Figure:
+        def val_err(t):
+            if isinstance(t, GroupVar):
+                return t.val_err()
+            nonlocal capsize
+            capsize = 0
+            return array(t), array([0] * len(t))
+
+        x_val, x_err = val_err(x)
+        y_val, y_err = val_err(y)
         self._scatters_kwargs.append(
-            dict(x=x_val, y=y_val, s=s, c=c, marker=marker, label=label))
+            dict(x=x_val, y=y_val, s=s, c=colour, marker=marker, label=label))
         self._errorbars_kwargs.append(
-            dict(x=x_val, y=y_val, xerr=x_err, yerr=y_err, capsize=capsize, capthick=1, fmt='none', c=c))
+            dict(x=x_val, y=y_val, xerr=x_err, yerr=y_err, capsize=capsize, capthick=1, fmt='none', c=colour))
+        return self
 
     def show(self):
         """
@@ -52,15 +121,19 @@ class Figure:
         axes = plt.figure().add_subplot()
         self._grid_lines(axes)
         self._show_plots(axes)
+        self._show_func_graphs_before_fixing_axes(axes)
         # maybe user wants to do something by himself
         if self.my_func is not None:
             self.my_func(axes)
         xy_limits = self._fix_axes(axes)
+        self._v_lines(axes, *xy_limits)
+        self._h_lines(axes, *xy_limits)
         self._set_label(axes)
         self._arrows(axes)
         if self.bold_axes is True:
             self._bold_axes(axes, *xy_limits)
-        self._show_lines(axes, *xy_limits)
+        self._show_lines(axes, self.legend_props, *xy_limits)
+        self._show_func_graphs_after_fixing_axes(axes)
         plt.show()
 
     @staticmethod
@@ -74,6 +147,10 @@ class Figure:
             axes.scatter(**scatter_kwargs)
             axes.errorbar(**errorbar_kwargs)
 
+    def _show_func_graphs_before_fixing_axes(self, axes):
+        for x, y, line_style, colour, label in self._func_graphs_before_fixing_axes:
+            axes.plot(x, y, color=colour, linestyle=line_style, label=label)
+
     def _fix_axes(self, axes):
         x_min, x_max = axes.get_xlim()
         y_min, y_max = axes.get_ylim()
@@ -84,28 +161,49 @@ class Figure:
         axes.set_ylim(y_min, y_max)
         return x_min, x_max, y_min, y_max
 
+    def _v_lines(self, axes, x_min, x_max, y_min, y_max):
+        for x, colour, line_style, label in self._v_lines_params:
+            if isinstance(x, Var):
+                x_val, x_err = x.val_err()
+                plot, = axes.plot((x_val, x_val), (y_min, y_max), color=colour, linestyle=line_style, label=label)
+                axes.plot((x_val - x_err, x_val - x_err), (y_min, y_max), color=plot.get_color(), linestyle=':')
+                axes.plot((x_val + x_err, x_val + x_err), (y_min, y_max), color=plot.get_color(), linestyle=':')
+                axes.fill_between((x_val - x_err, x_val + x_err), (y_min, y_min), (y_max, y_max),
+                                  color=plot.get_color(), alpha=self.alpha)
+            else:
+                axes.plot((x, x), (y_min, y_max), color=colour, linestyle=line_style, label=label)
+
+    def _h_lines(self, axes, x_min, x_max, y_min, y_max):
+        for y, colour, line_style, label in self._h_lines_params:
+            if isinstance(y, Var):
+                y_val, y_err = y.val_err()
+                plot, = axes.plot((x_min, x_max), (y_val, y_val), color=colour, linestyle=line_style, label=label)
+                axes.plot((x_min, x_max), (y_val - y_err, y_val - y_err), color=plot.get_color(), linestyle=':')
+                axes.plot((x_min, x_max), (y_val + y_err, y_val + y_err), color=plot.get_color(), linestyle=':')
+                axes.fill_between((x_min, x_max), (y_val - y_err, y_val - y_err), (y_val + y_err, y_val + y_err),
+                                  color=plot.get_color(), alpha=self.alpha)
+            else:
+                axes.plot((x_min, x_max), (y, y), color=colour, linestyle=line_style, label=label)
+
     def _set_label(self, axes):
-        if self.label_near_arrow is True:
-            y_label_prop = dict(rotation=0, y=1)
-            x_label_prop = dict(rotation=0, x=1)
-        else:
-            x_label_prop, y_label_prop = {}, {}
-        if self.x_label.rstrip() != '':
-            axes.set_xlabel('$' + self.x_label + '$', x_label_prop)
-        if self.y_label.rstrip() != '':
-            axes.set_ylabel('$' + self.y_label + '$', y_label_prop)
+        for set_label, axis, label, label_coords in ((axes.set_xlabel, axes.xaxis, self.x_label, self.x_label_coords),
+                                                     (axes.set_ylabel, axes.yaxis, self.y_label, self.y_label_coords)):
+            if label.rstrip() != '':
+                label_prop = {True: dict(rotation=0), False: {}}[self.label_near_arrow]
+                set_label('$' + label + '$', label_prop)
+                if self.label_near_arrow is True:
+                    axis.set_label_coords(*label_coords)
 
     def _arrows(self, axes):
-        axes.annotate('', xy=(1.05, 0), xycoords='axes fraction', xytext=(-0.03, 0),
-                      arrowprops=dict(arrowstyle=_mp.ArrowStyle.CurveB(head_length=1), color='black'))
-        axes.annotate('', xy=(0, 1.06), xycoords='axes fraction', xytext=(0, -0.03),
-                      arrowprops=dict(arrowstyle=_mp.ArrowStyle.CurveB(head_length=1), color='black'))
+        arrowprops = dict(arrowstyle=_mp.ArrowStyle.CurveB(head_length=1), color='black')
+        axes.annotate('', xy=(1.05, 0), xycoords='axes fraction', xytext=(-0.03, 0), arrowprops=arrowprops)
+        axes.annotate('', xy=(0, 1.06), xycoords='axes fraction', xytext=(0, -0.03), arrowprops=arrowprops)
 
     def _bold_axes(self, axes, x_min, x_max, y_min, y_max):
         axes.hlines(0, x_min, x_max, linewidth=1, colors='black')
         axes.vlines(0, y_min, y_max, linewidth=1, colors='black')
 
-    def _show_lines(self, axes, x_min, x_max, y_min, y_max):
+    def _show_lines(self, axes, legend_props, x_min, x_max, y_min, y_max):
         for k, b, c, ls, label in self._lines_params:
             points = []
             if y_min <= k * x_min + b <= y_max:
@@ -117,24 +215,57 @@ class Figure:
             if len(points) < 2 and x_min < (y_min - b) / k < x_max:
                 points.append(((y_min - b) / k, y_min))
             axes.plot((points[0][0], points[1][0]), (points[0][1], points[1][1]), c=c, ls=ls, label=label)
+        if len(axes.get_legend_handles_labels()[1]) != 0:
+            plt.legend(**legend_props)
+
+    def _show_func_graphs_after_fixing_axes(self, axes):
+        for x, y, line_style, colour, label in self._func_graphs_after_fixing_axes:
+            axes.plot(x, y, color=colour, linestyle=line_style, label=label)
 
 
-def mnk(x: GroupVar, y: GroupVar, figure: Optional[Figure] = None) -> Tuple[Var, Var]:
+def mnk(x: Union[GroupVar, Sequence], y: Union[GroupVar, Sequence], figure: Optional[Figure] = None,
+        colour: Optional[str] = None,
+        line_style: Optional[str] = None, label: Optional[str] = None) -> Tuple[Var, Var]:
+    # Данный метод считает два вида ошибок: вызываемый погрешностями и вызываемый статистикой.
+    # Если точки хорошо ложатся на прямую, то преобладать будет ошибка из-за погрешностей.
+    # Если точки измерены крайне точно, но на прямую они ложатся так себе, то преобладает статистическая ошибка.
+    # Рещультирующей ошибкой выдаётся корень из суммы квадратов двух видов этих ошибок
+    # TODO: учитывать точки с весом обратным квадрату ошибки
     if len(x) != len(y): raise TypeError('"x" and "y" must be the same length')
+    if len(x) == 0: raise ValueError('What should I do with no dots? Genius blyat!')
+    if len(x) == 1: raise ValueError('One dot!?!? Are you serious, Sam?')
+    if len(x) == 2: raise ValueError('There is no need in mnk if you have only 2 dots')
+    if not isinstance(x, GroupVar):
+        x = GroupVar(x, 0)
+    if not isinstance(y, GroupVar):
+        y = GroupVar(y, 0)
     x_sum: Var = reduce(lambda res, x_var: res + x_var, x)
     y_sum: Var = reduce(lambda res, y_var: res + y_var, y)
-    k: Var = (len(x) * reduce(lambda res, i: res + x[i] * y[i], range(len(x)), 0) - x_sum * y_sum) / \
-             (len(x) * reduce(lambda res, x_var: res + x_var * x_var, x, 0) - x_sum * x_sum)
-    b: Var = (y_sum - k * x_sum) / len(x)
+    k_ex: Var = (len(x) * reduce(lambda res, i: res + x[i] * y[i], range(len(x)), 0) - x_sum * y_sum) / \
+                (len(x) * reduce(lambda res, x_var: res + x_var * x_var, x, 0) - x_sum * x_sum)
+    b_ex: Var = (y_sum - k_ex * x_sum) / len(x)
+    k_stat_err, b_stat_err = _find_stat_errors(array(x.val()), array(y.val()), k_ex.val(), b_ex.val())
+    k, b = [Var(val, sqrt(stat_err ** 2 + exact_err ** 2)) for val, exact_err, stat_err in
+            [(*k_ex.val_err(), k_stat_err),
+             (*b_ex.val_err(), b_stat_err)]]
     if figure is not None:
-        figure.line(k.val(), b.val())
+        figure.line(k.val(), b.val(), colour=colour, line_style=line_style, label=label)
     return k, b
 
 
-def mnk_through0(x: GroupVar, y: GroupVar, figure: Optional[Figure] = None) -> Var:
+def mnk_through0(x: GroupVar, y: GroupVar, figure: Optional[Figure] = None, colour: Optional[str] = None,
+                 line_style: Optional[str] = None, label: Optional[str] = None) -> Var:
+    # todo: добавить статистическую ошибку
     if len(x) != len(y): raise TypeError('"x" and "y" must be the same length')
     k: Var = reduce(lambda res, i: res + x[i] * y[i], range(len(x)), 0) / \
              reduce(lambda res, x_var: res + x_var * x_var, x[1:], x[0] * x[0])
     if figure is not None:
-        figure.line(k.val(), 0)
+        figure.line(k.val(), 0, colour=colour, line_style=line_style, label=label)
     return k
+
+
+def _find_stat_errors(x: array, y: array, k, b):
+    Sy = sum((y - b - k * x) ** 2) / (len(x) - 2)
+    D = len(x) * sum(x ** 2) - (sum(x)) ** 2
+    # returns dk, db
+    return sqrt(Sy * len(x) / D), sqrt(Sy * sum(x ** 2) / D)
