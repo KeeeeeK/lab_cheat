4c0d6c5ad37825a0a5fdc634cbe74bcc0485167e
Vistog <viktor.korobkov.04@mail.ru>
2022-12-04 13:55:33 +0300

    Корректирован импорт data_exchanger

--- __init__.py
+++ __init__.py
@@ -4,6 +4,5 @@ from .table import TexTable, to_table
 from .plot import Figure, mnk, mnk_through0
 from .calculus_functions import sqrt, sin, cos, tg, ctg, arctg, arcctg, arcsin, arccos, \
                                 sh, ch, th, cth, arcth, arcsh, exp, ln, mean, step
-from .data_exchanger import read_data, shredder, get_into_groupvar_col_to_col, get_into_groupvar_col_named, \
-                            quick_use_form
+from .data_exchanger import *
 # Look in the advanced_functions file for additional functionality
