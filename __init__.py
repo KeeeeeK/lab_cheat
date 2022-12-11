453cd1bb07695e890d5a8d0326311d12f494b2cf
Vistog <viktor.korobkov.04@mail.ru>
2022-12-11 16:45:58 +0300

    data_exchanger добавлен в список импорта

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ from .table import TexTable, to_table
 from .plot import Figure, mnk, mnk_through0
 from .calculus_functions import sqrt, sin, cos, tg, ctg, arctg, arcctg, arcsin, arccos, \
                                 sh, ch, th, cth, arcth, arcsh, exp, ln, mean, step
-
+from .data_exchanger import *
 # Look in the advanced_functions file for additional functionality
